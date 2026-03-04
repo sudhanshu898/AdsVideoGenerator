79442e4fa5d1ff5de65d72abcc5614e04379b92d
sudhanshu898 <sudhanshu78787@gmail.com>
2026-03-04 18:45:25 +0530

    add new page of Create a image and video

--- client/src/components/UploadZone.tsx
+++ client/src/components/UploadZone.tsx
@@ -15,11 +15,11 @@ const UploadZone = ({label,file,onClear, onChange}: UploadZoneProps) => {
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity bg-black/40 rounded-xl backdrop-blur-sm">
                     <button type="button" onClick={onClear} className="p-2 rounded-full bg-white/10  hover:bg-red-500/20 text-white  hover:text-red-400 transition-colors">
 
-                        <XIcon className="w-5 h-5"/>
+                        <XIcon className="w-6 h-6"/>
                     </button>
                 </div>
 
-                <div className="absolute bottom-4 left-4 right-4 bg-black/50 backdrop-blur-md rounded-lg boreder border-white/10">
+                <div className="absolute bottom-4 left-4 right-4 bg-black/50 backdrop-blur-md p-3 rounded-lg boreder border-white/10">
                     <p className="text-sm font-medium truncate">{file.name}</p>
                 </div>
                 </>
