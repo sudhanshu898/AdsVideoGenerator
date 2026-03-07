46efe30284cf141af1679149ceadcdf9b8d441f3
sudhanshu898 <sudhanshu78787@gmail.com>
2026-03-07 14:24:56 +0530

    add some new file and codes

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,7 +1,21 @@
+import { Loader2Icon } from "lucide-react"
+import { useEffect } from "react"
 
 const Loading = () => {
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      window.location.href='/'
+    },6000)
+  },[])
+
   return (
-    <div>Loading</div>
+    <div className="h-screen flex flex-col">
+      <div className="flex items-center justify-center flex-1">
+        <Loader2Icon className="animate-spin size-7 text-indigo-200"/>
+
+      </div>
+    </div>
   )
 }
 
