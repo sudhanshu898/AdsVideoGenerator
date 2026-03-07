c8f68d37db67c0028a1897425faf97d2b0b9b99c
sudhanshu898 <sudhanshu78787@gmail.com>
2026-03-07 14:17:50 +0530

    add some new file and codes

--- client/src/pages/Plans.tsx
+++ client/src/pages/Plans.tsx
@@ -1,7 +1,14 @@
+import Pricing from "../components/Pricing"
 
 const Plans = () => {
   return (
-    <div>Plans</div>
+    <div className="max-sm:py-10 sm:pt-20">
+      <Pricing />
+
+      <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">Create stunning images for 
+        <span className="text-indigo-400 font-medium">5 credits</span> and generate immpresive videos for 
+        <span className="text-indigo-400 font-medium">10 credits</span>.</p>
+    </div>
   )
 }
 
