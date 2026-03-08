e316c24cacdb9f0fc79fd6bd59b5e2d341359a33
sudhanshu898 <sudhanshu78787@gmail.com>
2026-03-08 13:26:42 +0530

    add some new file and codes

--- server/controllers/clerk.ts
+++ server/controllers/clerk.ts
@@ -16,7 +16,7 @@ const clerkWebhooks= async (req :Request, res: Response) => {
                 await prisma.user.create({
                     data: {
                         id: data.id,
-                        email: data?.email_email_addresses[0]?.email_address,
+                        email: data?.email_addresses[0]?.email_address,
                         name: data?.first_name +" "+ data?.last_name,
                         image: data?.image_url, 
                     }
@@ -32,7 +32,7 @@ const clerkWebhooks= async (req :Request, res: Response) => {
                     },
                     data: {
                         
-                        email: data?.email_email_addresses[0]?.email_address,
+                        email: data?.email_addresses[0]?.email_address,
                         name: data?.first_name +" "+ data?.last_name,
                         image: data?.image_url, 
                     }
