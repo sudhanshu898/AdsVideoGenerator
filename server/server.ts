2f360a14a91b01e0ea593cc62d31ce258dce271b
sudhanshu898 <sudhanshu78787@gmail.com>
2026-03-08 12:32:46 +0530

    add some new file and codes

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,15 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerk.js";
 
 const app = express();
 
 // Middleware
 app.use(cors())
+
+app.post('/api/clerk', express.raw({ type: 'application/json' }), clerkWebhooks)
+
 app.use(express.json());
 app.use(clerkMiddleware())
 
@@ -16,6 +20,8 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
